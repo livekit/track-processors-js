0a4cf8d976879bd1f93e0288195b89b47ff5f073
Lukas <mail@lukasseiler.de>
2022-03-06 15:47:59 +0100

    use pipeline construction

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
+import BackgroundProcessor, { BlurOptions } from './BackgroundProcessor';
+import ProcessorPipeline from './ProcessorPipeline';
+
 export * from './BaseVideoProcessor';
+
+export const BackgroundBlur = (opts: BlurOptions) => {
+  const pipeline = new ProcessorPipeline([new BackgroundProcessor(opts)]);
+  return pipeline;
+};
