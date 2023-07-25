ece08c6d0e4902301b09102144f62efbeed602b4
kand193 <qkrdudwls99@gmail.com>
2023-07-25 12:38:39 +0900

    Upgrade livekit version, add example vite config

---
+++ example/vite.config.js
@@ -0,0 +1,13 @@
+// vite.config.js
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      }
+    }
+  }
+})
