dccc384b2334fa132279893502de39ec5a62d973
lukasIO <mail@lukasseiler.de>
2024-03-18 11:46:27 +0100

    Revert "Allow configurable asset paths for task vision assets"
    This reverts commit 834f2db5e439ac29c6824e8b8b867fca54272a4b.

--- src/transformers/VideoTransformer.ts
+++ src/transformers/VideoTransformer.ts
@@ -9,8 +9,6 @@ export default abstract class VideoTransformer<Options extends Record<string, un
 
   ctx?: OffscreenCanvasRenderingContext2D;
 
-  gl?: WebGL2RenderingContext;
-
   inputVideo?: HTMLVideoElement;
 
   protected isDisabled?: Boolean = false;
@@ -26,9 +24,8 @@ export default abstract class VideoTransformer<Options extends Record<string, un
       transform: (frame, controller) => this.transform(frame, controller),
     });
     this.canvas = outputCanvas || null;
-    if (this.canvas) {
-      this.ctx = this.canvas.getContext('2d') || undefined;
-      this.gl = this.canvas.getContext('webgl2') || undefined;
+    if (outputCanvas) {
+      this.ctx = this.canvas?.getContext('2d') || undefined;
     }
     this.inputVideo = inputVideo;
     this.isDisabled = false;
@@ -37,7 +34,7 @@ export default abstract class VideoTransformer<Options extends Record<string, un
   async restart({ outputCanvas, inputElement: inputVideo }: VideoTransformerInitOptions) {
     this.canvas = outputCanvas || null;
     this.ctx = this.canvas.getContext('2d') || undefined;
-    this.gl = this.canvas.getContext('webgl2') || undefined;
+
     this.inputVideo = inputVideo;
     this.isDisabled = false;
   }
