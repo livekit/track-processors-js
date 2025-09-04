77d154561a80d5434fa8b36ea227071315cda808
Ryan Gaus <rsg1egoman@gmail.com>
2025-09-04 14:23:13 -0400

    Cache background image (#100)

--- src/transformers/BackgroundTransformer.ts
+++ src/transformers/BackgroundTransformer.ts
@@ -36,7 +36,7 @@ export default class BackgroundProcessor extends VideoTransformer<BackgroundOpti
 
   segmentationResults: vision.ImageSegmenterResult | undefined;
 
-  backgroundImage: ImageBitmap | null = null;
+  backgroundImageAndPath: { imageData: ImageBitmap, path: string } | null = null;
 
   options: BackgroundOptions;
 
@@ -75,8 +75,8 @@ export default class BackgroundProcessor extends VideoTransformer<BackgroundOpti
     });
 
     // Skip loading the image here if update already loaded the image below
-    if (this.options?.imagePath && !this.backgroundImage) {
-      await this.loadBackground(this.options.imagePath).catch((err) =>
+    if (this.options?.imagePath) {
+      await this.loadAndSetBackground(this.options.imagePath).catch((err) =>
         console.error('Error while loading processor background image: ', err),
       );
     }
@@ -88,21 +88,24 @@ export default class BackgroundProcessor extends VideoTransformer<BackgroundOpti
   async destroy() {
     await super.destroy();
     await this.imageSegmenter?.close();
-    this.backgroundImage = null;
+    this.backgroundImageAndPath = null;
     this.isFirstFrame = true;
   }
 
-  async loadBackground(path: string) {
-    const img = new Image();
+  async loadAndSetBackground(path: string) {
+    if (!this.backgroundImageAndPath || this.backgroundImageAndPath?.path !== path) {
+      const img = new Image();
 
-    await new Promise((resolve, reject) => {
-      img.crossOrigin = 'Anonymous';
-      img.onload = () => resolve(img);
-      img.onerror = (err) => reject(err);
-      img.src = path;
-    });
-    const imageData = await createImageBitmap(img);
-    this.gl?.setBackgroundImage(imageData);
+      await new Promise((resolve, reject) => {
+        img.crossOrigin = 'Anonymous';
+        img.onload = () => resolve(img);
+        img.onerror = (err) => reject(err);
+        img.src = path;
+      });
+      const imageData = await createImageBitmap(img);
+      this.backgroundImageAndPath = { imageData, path };
+    }
+    this.gl?.setBackgroundImage(this.backgroundImageAndPath.imageData);
   }
 
   async transform(frame: VideoFrame, controller: TransformStreamDefaultController<VideoFrame>) {
@@ -200,7 +203,7 @@ export default class BackgroundProcessor extends VideoTransformer<BackgroundOpti
 
     this.gl?.setBlurRadius(opts.blurRadius ?? null);
     if (opts.imagePath) {
-      await this.loadBackground(opts.imagePath);
+      await this.loadAndSetBackground(opts.imagePath);
     } else {
       this.gl?.setBackgroundImage(null);
     }
